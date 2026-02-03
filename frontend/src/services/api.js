cc0eedf732ab1e1fecd677777d35db7ffd3bf339
sakthivel02022005-ctrl <sakthivel02022005@gmail.com>
2026-02-03 16:13:52 +0530

    cros

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://forexx-22183oplr-sakthivel-s-projects-ff0b17e9.vercel.app/api",
+  withCredentials: true
 });
 
 export default api;
