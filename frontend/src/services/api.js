121e50d80517b5da0904eb664141d0d542e45a28
sakthivel02022005-ctrl <sakthivel02022005@gmail.com>
2026-02-11 13:25:12 +0530

    df

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://forexx-22183oplr-sakthivel-s-projects-ff0b17e9.vercel.app/api",
+  baseURL: "https://forexx-flame.vercel.app",
   withCredentials: true
 });
 
